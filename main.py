b6f50d964b50fd4218be9f7b43b7f0814ede212f
shubhamatkal <shubhamatkal333@gmail.com>
2023-09-22 22:04:50 +0530

    Clean the code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+#imports
 from art import welcome_art
 import time
 import random
 
-
-
+#defining various functions
 def greet_users():
     print("Please wait...")
     time.sleep(2)
@@ -27,29 +27,26 @@ def checkwinner(x_place, o_place):
             return 0
     return -1
 
-
-
-
-
 greet_users()
 USER_X_NAME = str(input("What is the name of user X ? ")).title()
-USER_O_NAME = str(input("What is the name of user O ?")).title()
+USER_O_NAME = str(input("What is the name of user O ? ")).title()
 print("processing, pls wait ")
 time.sleep(1)
 print(f"USER X : {USER_X_NAME}")
 print(f"USER O : {USER_O_NAME}")
-is_x_ready = str(input(f"Is {USER_X_NAME} ready to start the game ? Type X , if not ready type EXIT").title())
+is_x_ready = str(input(f"Is {USER_X_NAME} ready to start the game ? Type X , if not ready type EXIT ").title())
 if is_x_ready == "X":
     global is_o_ready
-    is_o_ready = str(input(f"Is {USER_X_NAME} ready to start the game ? Type O , if not ready type EXIT").title())
+    is_o_ready = str(input(f"Is {USER_X_NAME} ready to start the game ? Type O , if not ready type EXIT ").title())
     if is_o_ready == "O":
         print("Ok so both of you are ready to play the game lets start")
     else:
         print(
-            f"user {USER_O_NAME} is not ready to play the game is aborted, {USER_X_NAME} pls tell your friend to be ready if you want to play the game , or find any other friend ;)")
+            f"user {USER_O_NAME} is not ready to play the game is aborted, {USER_X_NAME} pls tell your friend to be "
+            f"ready if you want to play the game , or find any other friend ;)")
 else:
-    print(f"user {USER_X_NAME} is not ready to play the game is aborted, {USER_O_NAME} pls tell your friend to be ready if you want to play the game , or find any other friend ;)")
-
+    print(f"user {USER_X_NAME} is not ready to play the game is aborted, {USER_O_NAME} pls tell your friend to be ready "
+          f"if you want to play the game , or find any other friend ;)")
 
 if is_o_ready == "O" and is_x_ready == "X":
     game_is_on = True
@@ -70,10 +67,10 @@ else:
     print(f"Hey {USER_O_NAME} its your turn")
     TURN = "O"
 
+print("INSTRUCTIONS:\n 1. The boxes are designed serially,\n 2.like 1,2,3,\n       4,5,6,\n       7,8,9\n3. you have "
+      "to type number where you want to put your mark")
 
-
-
-print("INSTRUCTIONS:\n 1. The boxes are designed serially,\n 2.like 1,2,3,\n       4,5,6,\n       7,8,9\n3. you have to type number where you want to put your mark")
+#staring the game
 while game_is_on == True:
     playground(x_place=x_place_list,o_place= o_place_list)
     if TURN == "X":
@@ -93,7 +90,6 @@ while game_is_on == True:
             continue
         TURN = "X"
 
-
     #last game ending logic
     cwin = checkwinner(x_place = x_place_list, o_place=o_place_list)
     if cwin == 1:
@@ -110,5 +106,3 @@ while game_is_on == True:
         print("Game is ENDED, and the result is DRAW :-)")
         playground(x_place=x_place_list, o_place=o_place_list)
         game_is_on = False
-
-
